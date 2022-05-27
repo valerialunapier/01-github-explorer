9b797736a16ba410318b745116653cce97420b07
valeria luna <valeria.luna@pier.digital>
2022-05-27 17:42:16 -0300

    criacao do primeiro componente e estilizacao dele

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+import { RepositoryItem } from './RepositoryItem'
+import '../styles/repositories.scss'
+import { useState, useEffect } from 'react'
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React', 
+    link: 'https://github.com/unform/unform' 
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => { 
+        fetch('https://api.github.com/users/valerialeticia/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
