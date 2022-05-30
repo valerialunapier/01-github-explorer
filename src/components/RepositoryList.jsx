37a4e9d136ebab10191d72b02ce3814f7ba018c9
valeria luna <valeria.luna@pier.digital>
2022-05-30 18:17:14 -0300

    adicao do typescript e deixando lista dinamica

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,12 +2,6 @@ import { RepositoryItem } from './RepositoryItem'
 import '../styles/repositories.scss'
 import { useState, useEffect } from 'react'
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React', 
-    link: 'https://github.com/unform/unform' 
-}
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([])
 
@@ -21,10 +15,11 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {
+                    repositories.map(repository => {
+                        return <RepositoryItem key={repository.name} repository={repository} />
+                    })
+                }          
             </ul>
         </section>
     )
